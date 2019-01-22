1bd280e8aaa3edf200080e71c068ba2336c5089f
Nathanous Trawets <natedawg1121@gmail.com>
2019-01-22 08:36:47 -0500

    Some basic setup stuff for chapter 11

--- src/Ch10/Exercises/TestLocation.java
+++ src/Ch10/Exercises/TestLocation.java
@@ -15,6 +15,6 @@ public class TestLocation {
         int col2 = gib.nextInt();
         Location location1 = new Location(row1, col1);
         Location location2 = new Location(row2, col2);
-        System.out.println("The distance between " + location1.toString() + " and " +location2.toString() + " is: " + Location.distance(location1, location2));
+        System.out.println("The distance between " + location1.toString() + " and " + location2.toString() + " is: " + Location.distance(location1, location2));
     }
 }
--- src/Ch11/IndexMaker/IndexMaker.java
+++ src/Ch11/IndexMaker/IndexMaker.java
@@ -14,62 +14,55 @@ import java.io.PrintWriter;
 import java.io.IOException;
 import java.util.Scanner;
 
-public class IndexMaker
-{
-  public static void main(String[] args) throws IOException
-  {
-    Scanner keyboard = new Scanner(System.in);
-    String fileName;
+public class IndexMaker {
+    public static void main(String[] args) throws IOException {
+        Scanner keyboard = new Scanner(System.in);
+        String fileName;
 
-    // Open input file:
+        // Open input file:
 
-    if (args.length > 0)
-      fileName = args[0];
-    else
-    {
-      System.out.print("\nEnter input file name: ");
-      fileName = keyboard.nextLine().trim();
+        if (args.length > 0)
+            fileName = args[0];
+        else {
+            System.out.print("\nEnter input file name: ");
+            fileName = keyboard.nextLine().trim();
+        }
+
+        BufferedReader inputFile = new BufferedReader(new FileReader(fileName), 1024);
+
+        // Create output file:
+
+        if (args.length > 1)
+            fileName = args[1];
+        else {
+            System.out.print("\nEnter output file name: ");
+            fileName = keyboard.nextLine().trim();
+        }
+
+        PrintWriter outputFile = new PrintWriter(new FileWriter(fileName));
+
+        // Create index:
+
+        DocumentIndex index = new DocumentIndex();
+
+        String line;
+        int lineNum = 0;
+        while ((line = inputFile.readLine()) != null) {
+            lineNum++;
+            index.addAllWords(line, lineNum);
+        }
+
+        // Save index:
+
+        for (IndexEntry entry : index)
+            outputFile.println(entry);
+
+        // Finish:
+
+        inputFile.close();
+        outputFile.close();
+
+        keyboard.close();
+        System.out.println("Done.");
     }
-
-    BufferedReader inputFile =
-                 new BufferedReader(new FileReader(fileName), 1024);
-
-    // Create output file:
-
-    if (args.length > 1)
-      fileName = args[1];
-    else
-    {
-      System.out.print("\nEnter output file name: ");
-      fileName = keyboard.nextLine().trim();
-    }
-
-    PrintWriter outputFile =
-                 new PrintWriter(new FileWriter(fileName));
-
-    // Create index:
-
-    DocumentIndex index = new DocumentIndex();
-
-    String line;
-    int lineNum = 0;
-    while ((line = inputFile.readLine()) != null)
-    {
-      lineNum++;
-      index.addAllWords(line, lineNum);
-    }
-
-    // Save index:
-
-    for (IndexEntry entry : index)
-      outputFile.println(entry);
-
-    // Finish:
-
-    inputFile.close();
-    outputFile.close();
-
-    keyboard.close();
-    System.out.println("Done.");
-  }
 }
