e29fc535c85d525cc8ce00414b50463561b4ced9
Nathanous Trawets <natedawg1121@gmail.com>
2019-02-20 13:31:34 -0500

    Put in the entirety of cotton-eyed joe for the bucket sorter to sort

--- src/Ch11/Exercises/ExerciseMethods.java
+++ src/Ch11/Exercises/ExerciseMethods.java
@@ -8,7 +8,7 @@ import java.util.List;
 class ExerciseMethods {
     ArrayList<String> testS = new ArrayList<>();
     ArrayList<Integer> testI = new ArrayList<>();
-    ArrayList<String> testW = new ArrayList<>();
+    List<String> testW = new ArrayList<>();
 
     ExerciseMethods() {
         testS.add("If it hadn't been for Cotton-Eye Joe");
@@ -21,20 +21,7 @@ class ExerciseMethods {
         testI.add(720);
         testI.add(7);
 
-        testW.add("If");
-        testW.add("it");
-        testW.add("hadn't");
-        testW.add("been");
-        testW.add("for");
-        testW.add("Cotton-Eyed");
-        testW.add("Joe");
-        testW.add("I'd");
-        testW.add("been");
-        testW.add("married");
-        testW.add("a");
-        testW.add("long");
-        testW.add("time");
-        testW.add("ago");
+        expandTestW();
     }
 
     ArrayList<String> reverseList(ArrayList<String> yeet) {
@@ -68,8 +55,9 @@ class ExerciseMethods {
         System.out.println(phil.toString());
     }
 
-    void wordBuckets(ArrayList<String> words) {
+    void wordBuckets(List<String> words) {
         char[] alphabet = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+        @SuppressWarnings("unchecked")
         ArrayList<String>[] buckets = new ArrayList[26];
         Arrays.setAll(buckets, ArrayList :: new);
         for (String word : words)
@@ -79,5 +67,78 @@ class ExerciseMethods {
         for (List high : buckets) {
             System.out.println(high.toString());
         }
+    }
+
+    private void expandTestW() {
+        String cottonEyedJoe = (
+        "If it hadn't been for Cotton-Eye Joe " +
+        "I'd been married a long time ago " +
+        "Where did you come from, where did you go? " +
+        "Where did you come from, Cotton-Eye Joe? " +
+
+        "If it hadn't been for Cotton-Eye Joe " +
+        "I'd been married a long time ago " +
+        "Where did you come from, where did you go? " +
+        "Where did you come from, Cotton-Eye Joe? " +
+
+        "If it hadn't been for Cotton-Eye Joe " +
+        "I'd been married a long time ago " +
+        "Where did you come from, where did you go? " +
+        "Where did you come from, Cotton-Eye Joe? " +
+
+        "If it hadn't been for Cotton-Eye Joe " +
+        "I'd been married a long time ago " +
+        "Where did you come from, where did you go? " +
+        "Where did you come from, Cotton-Eye Joe? " +
+
+        "He came to town like a midwinter storm " +
+        "He rode through the fields, so handsome and strong " +
+        "His eyes was his tools and his smile was his gun " +
+        "But all he had come for was having some fun " +
+
+        "If it hadn't been for Cotton-Eye Joe " +
+        "I'd been married a long time ago " +
+        "Where did you come from, where did you go? " +
+        "Where did you come from, Cotton-Eye Joe? " +
+
+        "If it hadn't been for Cotton-Eye Joe " +
+        "I'd been married a long time ago " +
+        "Where did you come from, where did you go? " +
+        "Where did you come from, Cotton-Eye Joe? " +
+
+        "He brought disaster wherever he went " +
+        "The hearts of the girls was to Hell, broken, sent " +
+        "They all ran away so nobody would know " +
+        "And left only men 'cause of Cotton-Eye Joe " +
+
+        "If it hadn't been for Cotton-Eye Joe " +
+        "I'd been married a long time ago " +
+        "Where did you come from, where did you go? " +
+        "Where did you come from, Cotton-Eye Joe? " +
+
+        "If it hadn't been for Cotton-Eye Joe " +
+        "I'd been married a long time ago " +
+        "Where did you come from, where did you go? " +
+        "Where did you come from, Cotton-Eye Joe? " +
+
+        "If it hadn't been for Cotton-Eye Joe " +
+        "I'd been married a long time ago " +
+        "Where did you come from, where did you go? " +
+        "Where did you come from, Cotton-Eye Joe? " +
+
+        "If it hadn't been for Cotton-Eye Joe " +
+        "I'd been married a long time ago " +
+        "Where did you come from, where did you go? " +
+        "Where did you come from, Cotton-Eye Joe? " +
+
+        "If it hadn't been for Cotton-Eye Joe " +
+        "I'd been married a long time ago " +
+        "Where did you come from, where did you go? " +
+        "Where did you come from, Cotton-Eye Joe?"
+        );
+
+        cottonEyedJoe = cottonEyedJoe.replaceAll("[?,]", "");
+        String arr[] = cottonEyedJoe.split(" ");
+        testW = Arrays.asList(arr);
     }
 }
